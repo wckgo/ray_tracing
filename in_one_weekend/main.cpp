f02f410fd8ce1127b243d482292a4e40fe88145f
wckgo <wckgogogo@gmail.com>
2020-07-10 13:20:00 +0800

    Rendering a red sphere

--- in_one_weekend/main.cpp
+++ in_one_weekend/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 
 color ray_color(const ray &r);
+bool hit_sphere(const point3 &center, double radius, const ray &r);
 
 int main()
 {
@@ -42,7 +43,21 @@ int main()
 
 color ray_color(const ray &r)
 {
+  if (hit_sphere(point3(0, 0, -1), 0.5, r))
+  {
+    return color(1, 0, 0);
+  }
   vec3 unit_direction = unit_vector(r.direction());
   auto t = 0.5 * (unit_direction.y() + 1.0);
   return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+bool hit_sphere(const point3 &center, double radius, const ray &r)
+{
+  vec3 oc = r.orgin() - center;
+  auto a = dot(r.direction(), r.direction());
+  auto b = 2.0 * dot(oc, r.direction());
+  auto c = dot(oc, oc) - radius * radius;
+  auto discriminant = b * b - 4 * a * c;
+  return (discriminant > 0);
 }
